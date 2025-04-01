47d7d6351406a729b43e0add05abec8bf80a8505
MafyssDB <hanterhanter599@gmail.com>
2025-04-01 11:56:15 +0700

    Create entities

---
+++ app/Http/Controllers/CarBrandController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CarBrand;
+use Illuminate\Http\Request;
+
+class CarBrandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CarBrand $carBrand)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CarBrand $carBrand)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CarBrand $carBrand)
+    {
+        //
+    }
+}
